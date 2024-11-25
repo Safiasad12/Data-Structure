3790cdb231fc546a63a550cc47070e67cab62e40
safi <safiasad12@gmail.com>
2024-11-25 10:38:50 +0530

    [safi] add : set

---
+++ set.js
@@ -0,0 +1,103 @@
+// let nums= new Set();
+
+// nums.add(2);
+// nums.add(3);
+// nums.add(2);
+
+// console.log(nums);
+
+
+// *************************************
+
+
+// const letters= new Set(['a', 'b', 'c', 'd']);
+
+// console.log(letters.has('c'));
+
+
+// ********************************************
+
+
+// let arr = [1,2,3,4,5,5,3];
+
+// let uniqueArr = new Set(arr);
+
+// console.log(uniqueArr);
+
+
+// **************************
+
+
+// let str = new Set("book keeper");
+
+// console.log(str);
+
+
+// ***********************************
+
+
+// let nums = new Set();
+
+// nums.add(1);
+// nums.add(3);
+// nums.add("safi");
+// nums.add("piyu");
+
+// nums.delete("safi");
+
+// console.log(nums);
+
+
+// **********************
+
+
+// const letters= new Set(['a', 'b', 'c', 'd']);
+
+// for(let letter of letters){
+//     console.log(letter);
+// }
+
+
+// **************************
+
+
+// const letters = new Set(['a', 'b', 'c', 'd']);
+
+// letters.forEach((letter)=>{
+//     console.log(letter);
+// });
+
+
+// ******************************
+
+
+// const letters = new Set(['a', 'b', 'c', 'd']);
+// console.log(letters.size);
+
+
+// ******************************
+
+
+// let nums= new Set();
+
+// nums.add(2);
+// nums.add(3);
+// nums.add(2);
+
+// console.log(Array.from(nums));
+
+
+// ******************************
+
+
+let mySet = new Set()
+
+mySet.add(1);
+mySet.add("safi");
+mySet.add(1.23);
+mySet.add({
+        namee :"vidvat",
+        age: 82
+    });
+
+console.log(mySet);
